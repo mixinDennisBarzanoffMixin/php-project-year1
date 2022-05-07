e6bb02c1c887200f5d4d7c726a3c02bbc268b4b2
DennisChickiboetsa <dennis.barzanoff@athlonsofia.com>
2022-05-07 13:02:02 +0200

    Add basic design

--- books.php
+++ books.php
@@ -3,6 +3,15 @@
     $sql = "select * from books;";
 
     $stm = $conn->query($sql);
+    echo '<div class="books">';
     foreach ($stm->fetchAll() as $row) {
-        echo '<div class="lol">'.$row[1].'</div>';
+        echo '<div class="book-card">';
+            echo '<div class="book-card__image"><img src="'.$row[2].'"/></div>';
+            echo '<h2 class="book-card__title">'.$row[1].'</h2>';
+            echo '<p class="book-card__genre">'.$row[4].'</p>';
+            echo '<p class="book-card__description">'.$row[3].'</p>';
+            echo '<button class="book-card__button-one">Update</button>';
+            echo '<button class="book-card__button-two">Delete</button>';
+        echo '</div>';
     }
+    echo '</div>';
--- database.sql
+++ database.sql
@@ -5,7 +5,11 @@ use books;
 create table books (
     isbn_id int auto_increment primary key,
     title varchar(255) not null,
-    link varchar(1000) not null
+    link varchar(1000) not null,
+    summary varchar(255) not null,
+    genre varchar(80) not null
 );
 
-insert into books values (null, 'Beauty and the Beast', 'https://google.com/'), (null, 'Beauty and the Abel', 'https://facebook.com');
+insert into books values 
+    (null, 'Beauty and the Beast', 'https://m.media-amazon.com/images/M/MV5BMTUwNjUxMTM4NV5BMl5BanBnXkFtZTgwODExMDQzMTI@._V1_.jpg', "An arrogant young prince (Robby Benson) and his castle's servants fall under the spell of a wicked enchantress, who turns him into the hideous Beast until he learns to love and be loved in return. ", "romance/fantasy"), 
+    (null, 'The Lion King', 'https://upload.wikimedia.org/wikipedia/en/9/9d/Disney_The_Lion_King_2019.jpg', "This Disney animated feature follows the adventures of the young lion Simba (Jonathan Taylor Thomas), the heir of his father, Mufasa (James Earl Jones). ", "adventury/drama");
