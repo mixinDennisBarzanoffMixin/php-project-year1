e28f9890c505216637a7c4f8e98919e1c8039957
DennisChickiboetsa <dennis.barzanoff@athlonsofia.com>
2022-05-08 12:19:50 +0200

    more css

--- books.php
+++ books.php
@@ -1,17 +1,33 @@
-<?php
-    include 'server.php';
-    $sql = "select * from books;";
+<div class="main">
+    <div class="main__left">
+        <div class="books">
+        <?php
+            include 'server.php';
+            $sql = "select * from books;";
 
-    $stm = $conn->query($sql);
-    echo '<div class="books">';
-    foreach ($stm->fetchAll() as $row) {
-        echo '<div class="book-card">';
-            echo '<div class="book-card__image"><img src="'.$row[2].'"/></div>';
-            echo '<h2 class="book-card__title">'.$row[1].'</h2>';
-            echo '<p class="book-card__genre">'.$row[4].'</p>';
-            echo '<p class="book-card__description">'.$row[3].'</p>';
-            echo '<button class="book-card__button-one">Update</button>';
-            echo '<button class="book-card__button-two">Delete</button>';
-        echo '</div>';
-    }
-    echo '</div>';
+            $stm = $conn->query($sql);
+                foreach ($stm->fetchAll() as $row) {
+                    echo '<div class="book-card">
+                            <div class="book-card__image"><img src="'.$row[2].'"/></div>
+                            <h2 class="book-card__title">'.$row[1].'</h2>
+                            <p class="book-card__genre">'.$row[4].'</p>
+                            <p class="book-card__description">'.$row[3].'</p>
+                            <button class="button book-card__button-one">
+                                <span class="button__text">Update</span>
+                            </button>
+                            <button class="button book-card__button-two">
+                                <span class="material-icons icon icon--small">delete</span>
+                                <span class="button__text">Delete</span>
+                            </button>
+                    </div>';
+                }
+            ?> 
+        </div>
+    </div>
+    <div class="main__right">
+        <a class="button">
+            <span class="material-icons icon icon--small">edit</span>
+            <span class="button__text">Create</span>
+        </a>
+    </div>
+</div>
